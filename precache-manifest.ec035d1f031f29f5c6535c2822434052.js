5bd30e8e62a67a70d8f7f2b2d9021f8d9cf79302
Noé Muñoz <noemuoz@hotmail.com>
2020-09-19 06:14:46 -0500

    Updates

---
+++ precache-manifest.ec035d1f031f29f5c6535c2822434052.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5499fc04399d861faf734affa74147e",
+    "url": "/intro-react/index.html"
+  },
+  {
+    "revision": "555f07540d1e9a138738",
+    "url": "/intro-react/static/css/main.592e4b49.chunk.css"
+  },
+  {
+    "revision": "b09e36b37530181673df",
+    "url": "/intro-react/static/js/2.6ac71704.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/intro-react/static/js/2.6ac71704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "555f07540d1e9a138738",
+    "url": "/intro-react/static/js/main.77bbf807.chunk.js"
+  },
+  {
+    "revision": "a51313cf8e282f836041",
+    "url": "/intro-react/static/js/runtime-main.b287a9c7.js"
+  },
+  {
+    "revision": "cb89af313821d877d3d6db3beb54613f",
+    "url": "/intro-react/static/media/profile.cb89af31.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.89a19a39af427d613dd1cfe698279e9f.js"
+  "/intro-react/precache-manifest.ec035d1f031f29f5c6535c2822434052.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/intro-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
